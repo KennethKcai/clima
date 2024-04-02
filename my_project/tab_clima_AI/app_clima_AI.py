38cebd723df8eac1bde46bb0599db3ed923203b2
KennethKcai <97841755+KennethKcai@users.noreply.github.com>
2024-04-02 16:19:05 -0700

    commit

--- my_project/tab_clima_AI/app_clima_AI.py
+++ my_project/tab_clima_AI/app_clima_AI.py
@@ -3,6 +3,12 @@ import dash_bootstrap_components as dbc
 
 def layout_clima_AI():
     return html.Div([
+        dbc.Row(
+            dbc.Col(
+                html.Button('AI Summary', id='ai-summary-btn', className='btn btn-secondary mb-3 btn-dark'),  
+                width={"size": 6, "offset": 3}  # 使按钮居中显示
+            )
+        ),
         dbc.Row([  # 创建一行
             dbc.Col(  # 创建一列
                 dcc.Input(
@@ -13,10 +19,11 @@ def layout_clima_AI():
                 ),
                 width=10  # 指定这列的宽度。Bootstrap 默认一行为 12 单位宽
             ),
-            dbc.Col(  
+            dbc.Col(
                 html.Button('Submit', id='new-tab-button', className='btn btn-primary btn-black'),
-                width=2  
+                width=2
             )
         ]),
-        html.Div(id='new-tab-output')  
+        html.Div(id='new-tab-output'),
+        html.Div(id='ai-summary-output')  # 用于显示AI总结的输出
     ])
